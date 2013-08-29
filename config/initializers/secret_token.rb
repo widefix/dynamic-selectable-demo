368c1aedbfcc9a354fd5fda378edc4a86814dc92
Andrey Koleshko <ka8725@gmail.com>
2013-08-29 12:06:26 +0300

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DynamicSelectableDemo::Application.config.secret_key_base = 'e34f95f0d1101e748d455f97832f8719f01efd84a43dbc3850b93fb354db2ba140cfa673b4c6f52318aa978956b749238b39fbe73e8da0efd040b59040584b3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DynamicSelectableDemo::Application.config.session_store :cookie_store, key: '_dynamic-selectable-demo_session'
